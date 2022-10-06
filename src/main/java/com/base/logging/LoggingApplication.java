2ade64eb70d1d59aeb74adba7609679762fb8cea
Oguzhan Ulusoy <oguzhan.ulusoy@orioninc.com>
2022-10-06 16:22:15 +0300

    some changes

--- src/main/java/com/base/logging/LoggingApplication.java
+++ src/main/java/com/base/logging/LoggingApplication.java
@@ -2,26 +2,19 @@ package com.base.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import com.base.logging.Log;
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @SpringBootApplication
 public class LoggingApplication {
 
-	final static String className = "com.base.logging.LoggingApplication";
-	static Log log = null;
+	private static final Logger logger = Logger.getLogger(String.valueOf(LoggingApplication.class));
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoggingApplication.class, args);
-		log = new Log();
-		writeLog("KOCAMAN HARFLERLE LOG BASIYORUM TAM BURAYA!!!");
+		JSONObject obj = new JSONObject();
+		obj.put("message","BURAYA BİR ADET LOG YAZDIM");
+		logger.info(obj);
 	}
-
-	private static void writeLog(String parameter){
-		log.info(parameter);
-		System.out.println("I wrote a log. Yey!");
-	}
-
 }
